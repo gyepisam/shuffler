fa8175c483524ba8609a069ddac0bb311152a68a
Gyepi Sam <self@gyepi.com>
2014-02-07 13:35:02 +0200

    First commit

---
+++ shuffler.go
@@ -0,0 +1,170 @@
+/*
+Package shuffler shuffles arrays of integers.
+
+Array entries can be free or anchored.
+The former type is shuffled and the latter type is not.
+An entry can be anchored by position and it retains the same position after shuffling.
+An entry could also be anchored relative its previous or next entry, in which case, it
+retains the same relative position to its anchor, which can be shuffled.
+
+Terminology
+"A > B" describes a list of two items where A is anchored to its successor, B
+"A B <" describes the inverse, where B is anchored to its predecessor, A.
+"A B . C" describes a list where B is anchored by position.
+
+
+Edge Cases:
+
+In "A > B <"  A and B are mutually anchored. This will be converted into "A > B"
+In "A < B C"  and "A B C >" The endpoints are anchored to non-existent neighbors. The anchors are removed.
+
+Chains of references are handled correctly.
+*/
+
+package shuffler
+
+import (
+	"math/rand"
+	"play/anchor"
+)
+
+// A shuffler shuffles integers (generally representing positions)
+// while maintaining anchored positions.
+type shuffler struct {
+	items    []int
+	position map[int]bool
+	before   map[int]int
+	after    map[int]int
+	skip     map[int]bool
+}
+
+// New returns an initialized shuffler.
+func New() *shuffler {
+	return &shuffler{
+		items:    make([]int, 0),
+		position: make(map[int]bool),
+		before:   make(map[int]int),
+		after:    make(map[int]int),
+		skip:     make(map[int]bool),
+	}
+}
+
+// Add adds a new items to be shuffled.
+// anchorType indicates how the item is anchored.
+func (s *shuffler) Add(slot int, anchorType anchor.Type) {
+	s.items = append(s.items, slot)
+	switch anchorType {
+	case anchor.Position:
+		s.position[slot] = true
+	case anchor.ToPrevious:
+		s.after[slot-1] = slot
+		s.skip[slot] = true
+	case anchor.ToNext:
+		s.before[slot+1] = slot
+		s.skip[slot] = true
+	}
+}
+
+// resolve dangling references and break mutual anchors
+// A < B C becomes A B C
+// A B C > becomes A B C
+// A > B < becomes A > B
+func (s *shuffler) resolve() {
+
+	if slot, ok := s.after[-1]; ok {
+		delete(s.after, -1)
+		delete(s.skip, slot)
+	}
+
+	if slot, ok := s.before[len(s.items)]; ok {
+		delete(s.before, len(s.items))
+		delete(s.skip, slot)
+	}
+
+	for from, to := range s.after {
+		if s.before[to] == from {
+			delete(s.before, to)
+			delete(s.skip, from)
+		}
+	}
+}
+
+// Shuffle shuffles and returns the list of items added with Add, using seed
+// to initialize the random number generator.
+func (s *shuffler) Shuffle(seed int64) []int {
+	l := len(s.items)
+	r := rand.New(rand.NewSource(seed))
+	p := r.Perm(l)
+	out := make([]int, l)
+	k := 0
+
+	s.resolve()
+
+	for _, j := range p {
+
+		// previous and next anchored items are established by their anchors.
+		if s.skip[j] {
+			continue
+		}
+
+		// position anchored items stay in place.
+		if s.position[j] {
+			out[j] = j
+			continue
+		}
+
+		// skip reserved position anchored slots
+		for s.position[k] {
+			k++
+		}
+
+		// insert "ToNext" anchored items before item.
+		// follow chains of references
+		for v, ok := s.before[j]; ok; v, ok = s.before[v] {
+			out[k] = v
+			k++
+		}
+
+		// place the item
+		out[k] = j
+		k++
+
+		// insert "ToPrevious" anchored items after item.
+		// follow chains of references
+		for v, ok := s.after[j]; ok; v, ok = s.after[v] {
+			out[k] = v
+			k++
+		}
+	}
+
+	return out
+}
+
+// old shuffle version. only handles position anchors, specified by an array.
+func shuffle(seed int64, items []int, anchored []int) []int {
+
+	anchorMap := make(map[int]bool)
+	for _, j := range anchored {
+		anchorMap[j] = true
+	}
+
+	l := len(items)
+	out := make([]int, l)
+	k := 0
+	r := rand.New(rand.NewSource(seed))
+	p := r.Perm(l)
+	for _, j := range p {
+		if anchorMap[j] {
+			out[j] = j
+			continue
+		}
+		for anchorMap[k] {
+			k++
+		}
+
+		out[k] = j
+		k++
+	}
+
+	return out
+}
---
+++ shuffler_test.go
@@ -0,0 +1,170 @@
+package shuffler
+
+import (
+	"math/rand"
+	"play/anchor"
+	"regexp"
+	"strings"
+	"testing"
+)
+
+type choice struct {
+	text       string
+	anchorType anchor.Type
+}
+
+var Cases = [][]choice{
+	// no choices
+	{},
+
+	// one choice, not anchored
+	{{"blue", anchor.None}},
+
+	// one choice, anchored
+	{{"banana", anchor.Position}},
+
+	// two choices, neither anchored
+	{{"male", anchor.None}, {"female", anchor.None}},
+
+	// two choices, last anchored
+	{{"male", anchor.None}, {"female", anchor.Position}},
+
+	// two choices, first anchored
+	{{"male", anchor.Position}, {"female", anchor.None}},
+
+	// two choices, both anchored
+	{{"male", anchor.Position}, {"female", anchor.Position}},
+
+	// three choices, middle one anchored
+	{{"coke", anchor.None}, {"water", anchor.Position}, {"tea", anchor.None}},
+
+	// four choices, none anchored
+	{{"internet", anchor.None}, {"television", anchor.None}, {"radio", anchor.None}, {"newspapers", anchor.None}},
+
+	// five choices, last one anchored
+	{{"mazda", anchor.None}, {"toyota", anchor.None}, {"miata", anchor.None}, {"ford", anchor.None}, {"none of the above", anchor.Position}},
+}
+
+func TestShuffle(t *testing.T) {
+	seed := rand.Int63()
+
+	for k, list := range Cases {
+		shuf := New()
+		items := make([]int, len(list))
+		anchored := make([]int, 0)
+		for i, choice := range list {
+			if choice.anchorType == anchor.Position {
+				anchored = append(anchored, i)
+			}
+			shuf.Add(i, choice.anchorType)
+			items[i] = i
+
+		}
+
+		out := shuf.Shuffle(seed)
+
+		for _, j := range anchored {
+			if out[j] != items[j] {
+				t.Errorf("%d: Failed. Expected matching anchor in pos %d, got %d", k, items[j], out[j])
+			}
+		}
+
+		t.Log("\nInput\n")
+		for i, choice := range list {
+			t.Logf("%d: %s %s\n", i, choice.text, choice.anchorType)
+		}
+		t.Log("\nOutput\n")
+		for _, j := range out {
+			choice := list[j]
+			t.Logf("%d: %s %s\n", j, choice.text, choice.anchorType)
+		}
+	}
+}
+
+type data struct {
+	name string
+	in   string //input
+	re   string //output regex
+}
+
+var anchored = []data{
+	{"empty", "", ""},
+	{"one choice, not anchored", "A", "A"},
+	{"one choice, anchored", "A.", "A"},
+	{"two choices, neither anchored", "AB", "AB|BA"},
+	{"two choices, last anchored", "AB.", "AB"},
+	{"two choices, first anchored", "A.B", "AB"},
+	{"two choices, both anchored", "A.B.", "AB"},
+	{"three choices, middle anchored", "AB.C", "ABC|CBA"},	
+	{"three choices, none anchored", "ABC", "ABC|BAC|BCA|CBA|CAB|ACB"},
+	{"three choices, first anchored", "A.BC", "A.."},
+	{"three choices, middle anchore", "AB.C", ".B."},
+	{"three choices, last anchored", "ABC.", "..C"},
+	{"Five choices, last anchored", "ABDCE.", "[^E]{4}E"},	
+	{"hold CD", "ABC.D.E", "[^CD]*CD[^CD]*"},
+	{"Dangling First", "A<B", "AB|BA"},
+	{"Dangling Last", "AB>", "AB|BA"},
+	{"Mutual anchor", "A>B<", "AB"},
+}
+
+func TestAnchored(t *testing.T) {
+	for i, v := range anchored {
+
+		s := strings.Map(func(r rune) rune {
+			if r == ' ' {
+				return -1
+			}
+			return r
+		}, v.in)
+
+		choices := make([]*choice, 0, len(s))
+		var top *choice
+
+		for _, r := range s {
+			switch r {
+			case '.', '<', '>':
+				if top == nil {
+					t.Fatalf("%d: incorrect input spec: %s %s", i, v.name, s)
+				}
+			}
+
+			switch r {
+			case '.':
+				top.anchorType = anchor.Position
+			case '>':
+				top.anchorType = anchor.ToNext
+			case '<':
+				top.anchorType = anchor.ToPrevious
+			default:
+				top = &choice{string(r), anchor.None}
+				choices = append(choices, top)
+			}
+		}
+
+		shuf := New()
+		for j, choice := range choices {
+			shuf.Add(j, choice.anchorType)
+		}
+		shuffled := shuf.Shuffle(rand.Int63())
+
+		tmp := make([]string, len(shuffled))
+		for j, k := range shuffled {
+			tmp[j] = choices[k].text
+		}
+		out := strings.Join(tmp, "")
+
+		t.Logf("%s -> %s\n", v.in, out)
+		
+		if want, got := len(choices), len(out); want != got {
+			t.Errorf("%d: wrong Count: expected output length=%d, got %d for %s", i, want, got, v.in)
+		}
+		
+		matched, err := regexp.MatchString(v.re, out)
+		if err != nil {
+			t.Fatal(err)
+		}
+		if !matched {
+			t.Errorf("NO MATCH. Want [%s] to match re [%s]\n", out, v.re)
+		}
+	}
+}
